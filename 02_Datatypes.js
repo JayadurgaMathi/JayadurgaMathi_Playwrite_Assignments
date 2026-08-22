184e384356ca1e3564b6614c462932b9bb55222f
JayadurgaMathi <786jayadurgadevi@gmail.com>
2026-08-22 09:24:31 +0530

    Add files via upload

---
+++ 02_Datatypes.js
@@ -0,0 +1,34 @@
+
+/* Datatype in JS:
+1.number
+2.string 
+3.Boolean
+4.Undefined
+5.Null
+*/
+var phoneNumber =   9159137713 //Initialization
+console.log(phoneNumber) 
+
+/*other pgm
+int x =123 
+String Data type*/
+var username = "aja@gmail.com" //we use double quotes, Single quotes, back tick
+console.log(username);
+console.log(typeof username);
+//we are using these symbols to understand JS that its dealing with s string datatype
+//Boolean data type - only true or falsevalues
+var isJavaScriptInteresting = true
+console.log(typeof phoneNumber);
+console.log(isJavaScriptInteresting);
+
+// 4.undefined
+
+var accountNumber 
+console.log(accountNumber); //undefined
+console.log(typeof accountNumber); // the datatype of variable that holds undefined as its value as "Undefined"
+
+/**************************************************** */
+//5.Null
+var landline = null;
+console.log(landline);
+console.log(typeof landline); //actually the datatype of null should be null but since JS developers create a big - historical bug during the development stage of JS datatype
